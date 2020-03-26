00eb9ef9286bc945c5e2a0df6f8998da2634697f
CHUNHUNGFAN <cs.chunhung.fan@smail.nchu.edu.tw>
2020-03-27 01:21:51 +0800

    [ADD] Initilization the ptt

---
+++ src/input.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+path = "dataset/HatePolitics/"
+allFileList = os.listdir(path)
+
+class Article():
+    def __init__(self, id):
+        self.id = id
+        self.user = []
+
+articles = []
+accounts = []
+
+for dataset in range(len(allFileList)):
+    with open(path + allFileList[dataset]) as file:
+        result = json.load(file)
+
+    # result: dictionary
+    counter = 0
+    for article in result["articles"]:
+        articles.append(Article(article["article_id"]))
+        
+        # article["messages"]: list
+        for comment in article["messages"]:
+            articles[counter].user.append(comment["push_userid"])
+            accounts.append(comment["push_userid"])
+
+        counter += 1
+
+account_duplicate = {i:accounts.count(i) for i in accounts}
+print(account_duplicate)
